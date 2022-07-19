45e9c94bfae57a2fe1711c9b7dc91c2b8c8ca765
tlziev <53547308+tlziev@users.noreply.github.com>
2022-07-19 13:14:50 -0400

    Add set up file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(name='utiliTZ',
+      version='0.0.0',
+      url='https://github.com/tlziev/utiliTZ',
+      description="",
+      author='Tracey L Ziev',
+      packages=find_packages(),
+      package_data={
+      },
+      install_requires=[
+      ],
+)
+
